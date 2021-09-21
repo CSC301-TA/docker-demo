7f2021369871f397e857f346e0ebbe8c91208d49
Jarrod Servilla <jarrod.servilla@utoronto.ca>
2021-09-21 13:45:17 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# docker-demo
+This code was originally created by Krish Chowdhary for the White Van Docker Workshop, 
+and modified by Jarrod Servilla for CSCC01 Summer 2021.
+
---
+++ docker/src/App.js
@@ -0,0 +1,17 @@
+import cube from './cute-cube.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={cube} alt="logo" />
+        <p>
+          Hello CSCC01!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
